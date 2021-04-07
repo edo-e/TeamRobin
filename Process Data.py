a89b1572e494bfbacb04eee8998079824b64c523
edo-e <80628301+edo-e@users.noreply.github.com>
2021-04-07 19:48:25 +0200

    Last updated

--- Process Data.py
+++ Process Data.py
@@ -2,52 +2,34 @@ import pandas as pd
 # , index_col=['Destination','Year']
 dataset_female = pd.read_csv(
     '/Users/eduardo/TeamRobin/UN_MigrantStockByOriginAndDestination_2019_md/Female.csv', sep=';', index_col=['Continent','Region','Destination', 'Year'])
-
 dataset_female.drop('Order',axis='columns', inplace=True)
-
 dataset_female_long = dataset_female.iloc[:, 9:-5].stack(level=-1).reset_index()
-
-dataset_female_long = dataset_female_long.rename(columns = {'level_4': 'Origin', 0: 'Flow'}, inplace = False)
-
+dataset_female_long = dataset_female_long.rename(columns = {'level_4': 'Origin', 0: 'Female'}, inplace = False)
 dataset_female_long['Gender']='Female' 
-
-dataset_female_long.to_csv('/Users/eduardo/TeamRobin/Female_origin_destination.csv')
-
-dataset_female_long_grouped_continent = dataset_female_long.groupby(['Continent','Year'])['Flow'].sum().reset_index()
-dataset_female_long_grouped_continent.to_csv('/Users/eduardo/TeamRobin/Female_continent.csv')
-
-dataset_female_long_grouped_region = dataset_female_long.groupby(['Region', 'Year'])['Flow'].sum().reset_index()
-dataset_female_long_grouped_region.to_csv('/Users/eduardo/TeamRobin/Female_region.csv')
-
-dataset_female_long_grouped_destination = dataset_female_long.groupby(['Destination', 'Year'])['Flow'].sum().reset_index()
-dataset_female_long_grouped_destination.to_csv('/Users/eduardo/TeamRobin/Female_destination.csv')
-
-dataset_female_long_grouped_destination = dataset_female_long.groupby(['Origin', 'Year'])['Flow'].sum().reset_index()
-dataset_female_long_grouped_destination.to_csv('/Users/eduardo/TeamRobin/Female_origin.csv')
+dataset_female_long.drop('Gender',axis='columns', inplace=True)
 
 dataset_male = pd.read_csv(
     '/Users/eduardo/TeamRobin/UN_MigrantStockByOriginAndDestination_2019_md/Male.csv', sep=';', index_col=['Continent','Region','Destination', 'Year'])
-
 dataset_male.drop('Order',axis='columns', inplace=True)
-
-print(dataset_male)
-
 dataset_male_long = dataset_male.iloc[:, 9:-5].stack(level=-1).reset_index()
-
-dataset_male_long = dataset_male_long.rename(columns = {'level_4': 'Origin', 0: 'Flow'}, inplace = False)
-
+dataset_male_long = dataset_male_long.rename(columns = {'level_4': 'Origin', 0: 'Male'}, inplace = False)
 dataset_male_long['Gender']='Male' 
+dataset_male_long.drop('Gender',axis='columns', inplace=True)
 
-dataset_male_long.to_csv('/Users/eduardo/TeamRobin/Male_origin_destination.csv')
+data_set_merged = pd.merge(dataset_male_long, dataset_female_long, on=['Destination','Origin', 'Year', 'Region', 'Continent'])
 
-dataset_male_long_grouped_continent = dataset_male_long.groupby(['Continent','Year'])['Flow'].sum().reset_index()
-dataset_male_long_grouped_continent.to_csv('/Users/eduardo/TeamRobin/Male_continent.csv')
+data_set_merged.to_csv('/Users/eduardo/TeamRobin/Total_origin_destination.csv')
 
-dataset_male_long_grouped_region = dataset_male_long.groupby(['Region', 'Year'])['Flow'].sum().reset_index()
-dataset_male_long_grouped_region.to_csv('/Users/eduardo/TeamRobin/Male_region.csv')
+data_set_merged_continent = data_set_merged.groupby(['Continent','Year'])[['Female','Male']].sum().reset_index()
+data_set_merged_continent.to_csv('/Users/eduardo/TeamRobin/Total_continent.csv')
 
-dataset_male_long_grouped_destination = dataset_male_long.groupby(['Destination', 'Year'])['Flow'].sum().reset_index()
-dataset_male_long_grouped_destination.to_csv('/Users/eduardo/TeamRobin/Male_destination.csv')
 
-dataset_male_long_grouped_destination = dataset_male_long.groupby(['Origin', 'Year'])['Flow'].sum().reset_index()
-dataset_male_long_grouped_destination.to_csv('/Users/eduardo/TeamRobin/Male_origin.csv')
+dataset_total_long_grouped_region = data_set_merged.groupby(['Region', 'Year'])[['Female','Male']].sum().reset_index()
+dataset_total_long_grouped_region.to_csv('/Users/eduardo/TeamRobin/Total_region.csv')
+
+dataset_total_long_grouped_destination = data_set_merged.groupby(['Destination', 'Year'])[['Female','Male']].sum().reset_index()
+dataset_total_long_grouped_destination.to_csv('/Users/eduardo/TeamRobin/Total_destination.csv')
+
+dataset_total_long_grouped_origin = data_set_merged.groupby(['Origin', 'Year'])[['Female','Male']].sum().reset_index()
+dataset_total_long_grouped_origin.to_csv('/Users/eduardo/TeamRobin/Total_origin.csv')
+print(dataset_total_long_grouped_origin.head())
